10d390ac292585d7476deffe419d6bc63a99d9fb
Lakshya <lakshyam38@gmail.com>
2023-03-31 13:22:58 +0530

    Add user schemas

--- src/schemas.py
+++ src/schemas.py
@@ -21,3 +21,27 @@ class RideResponse(RideBase):
 
     class Config:
         orm_mode = True
+
+
+# User
+class UserBase(BaseModel):
+    UID: int
+    fname: str
+    lname: str
+    email: str
+    designation: str
+    user_type: str
+    phone: int
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserResponse(UserBase):
+    pass
+
+
+class UserLogin(BaseModel):
+    UID: int
+    password: str
