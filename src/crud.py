26000e68ffa0dc0c0770bfff9d436278c47cbe6f
Lakshya <lakshyam38@gmail.com>
2023-03-31 13:23:40 +0530

    Add user functions

--- src/crud.py
+++ src/crud.py
@@ -2,7 +2,7 @@ import uuid
 
 from sqlalchemy.orm import Session
 
-from . import models, schemas
+from . import models, schemas, utils
 
 
 def create_ride(db: Session, ride: schemas.RideCreate):
@@ -22,3 +22,28 @@ def get_all_rides(db: Session):
 
 def get_ride_by_id(db: Session, ride_id: str):
     return db.query(models.Ride).filter(models.Ride.id == uuid.UUID(ride_id)).first()
+
+
+# users
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = utils.hash_password(user.password)
+    db_user = models.User(
+        UID=user.UID,
+        fname=user.fname,
+        lname=user.lname,
+        email=user.email,
+        designation=user.designation,
+        user_type=user.user_type,
+        phone=user.phone,
+        hashed_password=hashed_password,
+    )
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
+
+
+def get_user_by_id(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.UID == user_id).first()
--- src/main.py
+++ src/main.py
@@ -39,6 +39,19 @@ def read_ride(ride_id: str, db: Session = Depends(get_db)):
     return crud.get_ride_by_id(db=db, ride_id=ride_id)
 
 
+@app.post("/users/register", status_code=201)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_id(db=db, user_id=user.UID)
+    if db_user:
+        raise HTTPException(
+            status_code=400,
+            detail="User already exists with the provided ID!",
+        )
+
+    db_user = crud.create_user(db=db, user=user)
+    return db_user
+
+
 @app.get("/healthcheck")
 def health_check():
     return {"status": "OK"}
