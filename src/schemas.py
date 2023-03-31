6a9061db3e3ce5e1ca612510b5d5b538755d354e
Lakshya <lakshyam38@gmail.com>
2023-03-31 12:58:52 +0530

    Add request and base schemas for rides

--- src/schemas.py
+++ src/schemas.py
@@ -1,10 +1,23 @@
+import uuid
+
 from pydantic import BaseModel
 
 
-class RideCreate(BaseModel):
+class RideBase(BaseModel):
     publisher_id: str
     from_location: str
     to_location: str
     passenger_count: int
     doj: str
     price: int
+
+
+class RideCreate(RideBase):
+    pass
+
+
+class RideResponse(RideBase):
+    id: uuid.UUID
+
+    class Config:
+        orm_mode = True
