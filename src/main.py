66d8b9b3bf71a4bcd20f07ea13621f9bc2f15c56
Lakshya <lakshyam38@gmail.com>
2023-03-31 20:14:33 +0530

    Added functions for ride request buffer

--- src/main.py
+++ src/main.py
@@ -18,27 +18,20 @@ def get_db():
         db.close()
 
 
-# HTTP POST /rides -> create a new ride
-@app.post("/rides")
-def publish_ride(ride: schemas.RideCreate, db: Session = Depends(get_db)):
-    db_ride = crud.create_ride(db=db, ride=ride)
-
-    return db_ride
-
-
-# HTTP GET /rides -> get all rides
-@app.get("/rides")
+# Rides API
+@app.get("/rides", response_model=list[schemas.RideResponse])
 def read_rides(db: Session = Depends(get_db)):
     db_rides = crud.get_all_rides(db=db)
 
     return db_rides
 
 
-@app.get("/rides/{ride_id}")
+@app.get("/rides/{ride_id}", response_model=schemas.RideResponse)
 def read_ride(ride_id: str, db: Session = Depends(get_db)):
     return crud.get_ride_by_id(db=db, ride_id=ride_id)
 
 
+# Users API
 @app.post("/users/register", status_code=201)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_id(db=db, user_id=user.UID)
@@ -50,6 +43,45 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
     db_user = crud.create_user(db=db, user=user)
     return db_user
+
+
+@app.get("/users/{user_id}", response_model=schemas.UserResponse)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_id(db=db, user_id=user_id)
+    if not db_user:
+        raise HTTPException(
+            status_code=404,
+            detail="Requested user not found!",
+        )
+
+    return db_user
+
+
+@app.get("/users/{user_id}/rides")
+def read_rides_for_user(user_id: int, db: Session = Depends(get_db)):
+    rides = crud.get_rides_by_publisher_id(db=db, publisher_id=user_id)
+
+    return rides
+
+
+@app.post("/users/{user_id}/rides")
+def publish_ride_for_user(
+    user_id: int, ride: schemas.RideCreate, db: Session = Depends(get_db)
+):
+    db_ride = crud.create_ride(db=db, ride=ride, user_id=user_id)
+
+    return db_ride
+
+
+@app.post("/users/{user_id}/requests")
+def create_ride_request(
+    user_id: int, ride_request: schemas.RideRequestCreate, db: Session = Depends(get_db)
+):
+    if not (crud.get_user_by_id(user_id=ride_request.publisher_id, db=db)):
+        raise HTTPException(status_code=400, detail="Ride or Publisher doesn't exist!")
+    return crud.create_ride_request(
+        db=db, ride_request=ride_request, requestee_id=user_id
+    )
 
 
 @app.get("/healthcheck")
