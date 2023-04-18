d02ed56056ee4a11abb37b1939e2727d8f4a5eb5
Lakshya <lakshyam38@gmail.com>
2023-04-18 14:44:45 +0530

    correction

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 RUN_PORT=${PORT:-8000}
 
-gunicorn -k uvicorn.workers.UvicornWorker src.api:app -b '0.0.0.0':${RUN_PORT}
+gunicorn -k uvicorn.workers.UvicornWorker src.main:app -b '0.0.0.0':${RUN_PORT}
